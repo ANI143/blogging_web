73085bd2f51de1872a1a06e663eb1481726f9f8a
ANI143 <aniruddhasharma824@gmail.com>
2024-09-05 20:38:03 +0530

    index file me chenges

--- src/index.js
+++ src/index.js
@@ -4,17 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './pages/AuthContext'; // Import the AuthProvider
 import './index.css'; // Import global styles if any
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <Router>
-//     <App />
-//   </Router>
-// );
-
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
